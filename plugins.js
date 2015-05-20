431cd4af8209dfb6c8cc813be4ae9a2d374e3847
fengb <contact@fengb.info>
2015-05-19 20:56:16 -0500

    Added chai-generator

--- plugins.js
+++ plugins.js
@@ -304,4 +304,12 @@ module.exports = [
     , markdown: 'https://raw.githubusercontent.com/astorije/chai-immutable/master/README.md'
     , browser: false
     }
+
+  , { name: 'Chai Generator'
+    , desc: 'Extend Chai with assertions for Javascript generators'
+    , url: 'chai-generator'
+    , link: 'https://github.com/fengb/chai-generator'
+    , tags: [ 'assertion', 'generator' ]
+    , pkg: 'https://raw.githubusercontent.com/fengb/chai-generator/master/package.json'
+    , markdown: 'https://raw.githubusercontent.com/fengb/chai-generator/master/README.md' }
 ];

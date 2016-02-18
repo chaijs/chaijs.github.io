98812ed578eba830d736a2300a7b2bd89aca4c39
Keith Cirkel <github@keithcirkel.co.uk>
2016-02-18 16:14:12 +0000

    feat(plugins): Add dedicated plugin pages

---
+++ _scripts/build-plugin-pages.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var files = fs.readdirSync('_data/plugins');
+files.filter(function (file) {
+  return file.slice(-5) === '.json';
+}).forEach(function (file) {
+  try {
+    var name = file.slice(0, -5);
+    var pluginJson = JSON.parse(fs.readFileSync('_data/plugins/' + file, 'utf8'));
+    var contents = '---\nlayout: plugin\npermalink: plugins/' + name + '/\npluginName: ' + name + '\n---\n\n' + pluginJson.readme;
+    fs.writeFileSync(__dirname + '/../plugins/' + name + '.md', contents, 'utf8');
+    console.log('Wrote', 'plugins/' + file + '.md')
+  } catch(e) {
+    console.error(e.stack || e);
+    process.exit(1);
+  }
+});

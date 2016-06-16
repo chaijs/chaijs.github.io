72c02130deca438bb56cac11a46b7a8720bf1923
Keith Cirkel <keithamus@users.noreply.github.com>
2016-06-16 09:56:14 +0100

    Ban some plugins from the plugin listing

--- _scripts/banned_plugins.js
+++ _scripts/banned_plugins.js
@@ -2,5 +2,7 @@ module.exports = [
 
   'chai-51st-state', // Banned for profane language
   'chai2.0-webdriver-promised', // Fork with poor metadata
+  'lodash-match-pattern', // not a real chai plugin, chai-match-pattern is the real one
+  'shimo-chai-as-promised', // dupe of chai-as-promised
 
 ];

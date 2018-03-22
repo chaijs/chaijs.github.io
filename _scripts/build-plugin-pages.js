3644aa9ea0642472cf4b1ff4c1d6265894dbb7a2
Keith Cirkel <keithamus@users.noreply.github.com>
2018-03-22 15:17:42 +0000

    fix(build-plugin-pages): variable name casing

--- _scripts/build-plugin-pages.js
+++ _scripts/build-plugin-pages.js
@@ -6,7 +6,7 @@ files.filter(function (file) {
   try {
     var name = file.slice(0, -5);
     var pluginJson = JSON.parse(fs.readFileSync('_data/plugins/' + file, 'utf8'));
-    var contents = '---\nlayout: plugin\npermalink: plugins/' + pluginJson.name + '/\npluginName: ' + pluginJSON.name + '\n---\n\n' + pluginJson.readme;
+    var contents = '---\nlayout: plugin\npermalink: plugins/' + pluginJson.name + '/\npluginName: ' + pluginJson.name + '\n---\n\n' + pluginJson.readme;
     fs.writeFileSync(__dirname + '/../plugins/' + name + '.md', contents, 'utf8');
     console.log('Wrote', 'plugins/' + name + '.md')
   } catch(e) {

24af0083ee0ec7bafe117fec4e9c09dd4e567575
Keith Cirkel <keithamus@users.noreply.github.com>
2018-05-15 13:00:31 +0200

    chore: blacklist personal forks of plugins

--- _scripts/banned_plugins.js
+++ _scripts/banned_plugins.js
@@ -1,5 +1,9 @@
 module.exports = [
 
+   // Personal forks
+  '@cypress/sinon-chai',
+  '@slightlytyler/sinon-chai',
+
   'chai-51st-state', // Banned for profane language
   'chai2.0-webdriver-promised', // Fork with poor metadata
   'lodash-match-pattern', // not a real chai plugin, chai-match-pattern is the real one
